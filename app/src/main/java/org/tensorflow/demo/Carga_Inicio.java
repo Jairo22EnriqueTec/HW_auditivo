c8cca9e32a217ed5b300e8c491d90061eb490ecc
Jairo22EnriqueTec <a01750443@itesm.mx>
2020-03-01 12:37:08 -0600

    01/03

--- app/src/main/java/org/tensorflow/demo/Carga_Inicio.java
+++ app/src/main/java/org/tensorflow/demo/Carga_Inicio.java
@@ -18,6 +18,7 @@ public class Carga_Inicio extends AppCompatActivity {
             @Override
             public void run() {
                 String dele="DELETE FROM Usuario";
+                //Funcionó
               //  this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 //setContentView(R.layout.activity_main);
 
